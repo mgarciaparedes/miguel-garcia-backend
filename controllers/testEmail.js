eaf09c26eaa42d38671ace903a77f702288d4029
mgarciaparedes <46930317+mgarciaparedes@users.noreply.github.com>
2023-01-16 19:21:37 -0300

    fix: switch sendgrid to nodemailer

--- controllers/testEmail.js
+++ controllers/testEmail.js
@@ -1,38 +1,63 @@
 const { response } = require("express");
-const sendgridMail = require("@sendgrid/mail");
-sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
+const nodemailer = require('nodemailer');
 
 const postTestEmail = async (req, res = response) => {
   const {fullname, email, subject, message} = req.body;
 
-  const msg = {
+  const transporter = nodemailer.createTransport({
+    host: 'mail.miguel-garcia.site',
+    port: 465,
+    auth: {
+        user: process.env.NODEMAILER_API_USER,
+        pass: process.env.NODEMAILER_API_PASS
+    }
+});
+
+  const emailMsg = {
     to: "miguelgarciaparedes22@gmail.com",
-    from: "info@stdicompany.com",
+    from: "info@miguel-garcia.site",
     subject: subject,
     //text: "Contact form from miguel-garcia.site",
     html: "Hi <b>" +
     fullname +
     "</b>, with email "+ email +", has sent you a message: <b>" + message +
     ".<br/> Reminder: please answer to the customer."
-  }
+  };
 
-  await sendgridMail
-    .send(msg)
-    .then((response) => {
-      console.log(response[0].statusCode);
-      console.log(response[0].headers);
-      res.json({
-        ok: true,
-        msg: "Mail has been sent"
-      });
-    })
-    .catch((error) => {
+  transporter.sendMail(emailMsg, function(error, success){
+    if (error) {
       console.error(error);
-      res.status(500).json({
-        ok: false,
-        msg: "Error"
-      });
-    });
+          res.status(500).json({
+            ok: false,
+            msg: "Error"
+          });
+    } else {
+        console.log('Nodemailer Email sent: ' + success.response);
+        res.json({
+          ok: true,
+          msg: "Mail has been sent"
+        });
+    }
+  });
+
+
+  // await sendgridMail
+  //   .send(msg)
+  //   .then((response) => {
+  //     console.log(response[0].statusCode);
+  //     console.log(response[0].headers);
+  //     res.json({
+  //       ok: true,
+  //       msg: "Mail has been sent"
+  //     });
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //     res.status(500).json({
+  //       ok: false,
+  //       msg: "Error"
+  //     });
+  //   });
 
 };
 
